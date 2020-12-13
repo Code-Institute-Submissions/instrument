35fe66a46cbae7b3614060ba16954c207cf1f101
sammckenna1986 <sammckenna1986@gmail.com>
2020-12-13 16:13:20 +0000

    added the extra admin functionality in the product section in terms of displaying more of the infomration and also added the freindly name of the product as perscribed in the videos.

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
+
+# This changes the display of the normal django default of the displayed attributes.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+# This displays the friendly name that we predefined to make it nicer to read.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 admin.site.register(Product)
 admin.site.register(Category)
