c2dd559e8ab1f0f79bc7fea6fc90800c60712d64
sammckenna1986 <sammckenna1986@gmail.com>
2020-12-13 16:42:40 +0000

    followed the instructions to create a product html template and put in the products template variable and I am going to check if it is connecting correctly to the backend

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def all_products(request):
+    """ A view to show the products, the search function and sorting function """
+    #This imports all the products
+    products = Product.objects.all()
+
+    #This sets the context as the products that we are passing through the products template.
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'home/products.html', context)
+# Copied from the home views.py but it is important to add in the context as we will be passing items through.
