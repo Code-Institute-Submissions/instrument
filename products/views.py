cfbccd00ef3e4ccc6ccca2bd8e58aa0c13827eec
sammckenna1986 <sammckenna1986@gmail.com>
2020-12-21 16:25:34 +0000

    added product detail section as per instructional video

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 # Create your views here.
 
@@ -13,4 +13,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+# Copied from the home views.py but it is important to add in the context as we will be passing items through.
+
+def product_detail(request, product_id):
+    """ A view to show the product detail. """
+    #This imports all the products
+    product = get_object_or_404(Product, pk=product_id)
+
+    #This sets the context as the products that we are passing through the products template.
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
 # Copied from the home views.py but it is important to add in the context as we will be passing items through.
