4377c15f5a2bcad6d598fb552e2ed07994ed7b19
sammckenna1986 <sammckenna1986@gmail.com>
2021-01-03 17:29:46 +0000

    added messaging levels to the website but the adjust and the removal of items from the shopping cart are not working right now. I have to still figure out why and fix it.

--- bag/views.py
+++ bag/views.py
@@ -33,31 +33,34 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
-
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
     
     bag = request.session.get('bag', {})
 
  
     if quantity > 0:
+        messages.success(request, f'Updated {product.name} quantity in your bag')
         bag[item_id] = quantity
     else:
+        messages.success(request, f'Removed {product.name} from your bag')
         bag.pop(item_id)
-
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
-
+    product = get_object_or_404(Product, pk=item_id)
     try:
         bag = request.session.get('bag', {})
 
+        messages.success(request, f'Removed {product.name} from your bag')
         bag.pop(item_id)
-
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
