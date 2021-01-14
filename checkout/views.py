89076db288ffbe814d30399565662c682373c5f0
sammckenna1986 <sammckenna1986@gmail.com>
2021-01-14 16:32:07 +0000

    added the stripe_elements.js, checkout postload js, the checkout.css content, as well as the stripe script to the base.html file following the instructional videos

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51I9YB5E9u9fEGOsf6STzF6iPgmQrUWW6JGEchobeySk7EM4vZQ8bNZXkTk6GI3Fqhicl5p0SQKTK4SudeGgNtrOS006THLXVTx',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
