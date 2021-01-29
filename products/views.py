99a3201be969b63f1206b5de4e07868ec6b342e2
sammckenna1986 <sammckenna1986@gmail.com>
2021-01-29 17:30:03 +0000

    created the template to be able to add products as per the instructional videos

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
-
-
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
+from .forms import ProductForm
+
 # Create your views here.
 
 def all_products(request):
@@ -12,8 +14,24 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -28,24 +46,36 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
-# Copied from the home views.py but it is important to add in the context as we will be passing items through.
+
 
 def product_detail(request, product_id):
-    """ A view to show the product detail. """
-    #This imports all the products
+    """ A view to show individual product details """
+
     product = get_object_or_404(Product, pk=product_id)
 
-    #This sets the context as the products that we are passing through the products template.
     context = {
         'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
-# Copied from the home views.py but it is important to add in the context as we will be passing items through.
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
