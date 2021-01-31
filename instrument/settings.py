9dc4c00ce07344c9a52e057ef4134908f09fb266
sammckenna1986 <sammckenna1986@gmail.com>
2021-01-31 14:56:56 +0000

    trying to change the allowed hosts to ALLOWED_HOSTS = ['instrument.herokuapp.com', 'localhost']

--- instrument/settings.py
+++ instrument/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['instrument-shop.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['instrument.herokuapp.com', 'localhost']
 
 
 # Application definition
